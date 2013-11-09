92c105b06a1d85776f7dfe067a7c174b8b8094db
Daniel Schwen <daniel@schwen.de>
2013-11-09 14:52:18 -0700

    add shedding of privileges

--- bin/servlet.js
+++ bin/servlet.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 var configFile = process.argv[2]
-  , port = parseInt(process.argv[3], 10)
+  , port = parseInt(process.argv[3] || '80', 10)
+  , ugid = process.argv[4] || null
   , fs = require('fs')
   , Notify = require('fs.notify')
   , servlet = require('../')
@@ -9,13 +10,24 @@ var configFile = process.argv[2]
   ;
 
 if (!configFile || !port) {
-    console.error('Usage: servlet [hosts.json] [port]');
+    console.error('Usage: servlet hosts.json [port] [uid:gid]');
     process.exit(1);
 }
 
 // load config file
 var config = JSON.parse(fs.readFileSync(configFile));
 servlet.init( config, port );
+
+// drop root priviliges
+if( process.getuid() == 0 ) {
+  if( ugid===null) {
+    throw "Refusing to run as root, please supply user and group to change to.";
+  }
+
+  var ug=ugid.split(':');
+  process.setuid(ug[0]);
+  process.setgid(ug[1]);
+}
 
 // set up watch on config file
 var notifier = new Notify( configFile );
